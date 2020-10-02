2c40abf498bf63778ccaa9c458d5a4bca0c9d551
Davide Castellani <38205382+DavideC03@users.noreply.github.com>
2020-10-02 09:28:36 +0200

    Created bin/template.py

---
+++ bin/usb.py
@@ -0,0 +1,19 @@
+"""usb
+"""
+
+__author__ = "help@castellanidavide.it"
+__version__ = "1.0 2020-10-2"
+
+class usb:
+	def __init__ (self):
+		"""Where it all begins
+		"""
+		print(usb.usb())
+	
+	def usb():
+		"""usb first funtion
+		"""
+		return "usb" #for the test (see test_usb.py)
+		
+if __name__ == "__main__":
+	usb()
